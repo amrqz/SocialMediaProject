ab9c52aac31f37b8539a6208384b6ea48a2d8022
Hattie-B <Hattie-B@users.noreply.github.com>
2021-04-09 12:50:25 +0100

    Made another fix

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import { PostCreator, PostDisplay } from './PostCreator.jsx'
+import { PostDisplay } from './PostCreator.jsx'
 
-let postsHere = [{text: "Hi this is a post", time: "12:26:47", user: "me"}, {text: "Hi this is a post", time: "12:26:47", user: "me"}];
+const postsHere = [{text: "Hi this is a post", time: "12:26:47", user: "me"}, {text: "Hi this is a post", time: "12:26:47", user: "me"}];
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Social Media Project</h1>
       </header>
-      <PostDisplay postsNow={postsHere} />
+      <PostDisplay postsNow={postsHere} user="aelyssa" />
       <AmplifySignOut />
     </div>
   );
