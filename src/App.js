7fa0dd8ba64a1287ba79530d8c02632f48973872
Hattie-B <Hattie-B@users.noreply.github.com>
2021-04-12 12:29:53 +0100

    Added front end code for using GraphQL

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
-import { PostDisplay } from './PostCreator.jsx'
-
+import { API } from 'aws-amplify';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation } from './graphql/mutations';
+import { Auth } from 'aws-amplify';
+import { PostDisplay, PostCreator } from './PostCreator.jsx';
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+  const [formText, setFormText] = useState('');
+  
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  async function fetchPosts() {
+    const apiData = await API.graphql({ query: listNotes });
+    setPosts(apiData.data.listNotes.items);
+  }
+
+  async function createPosts() {
+    const user = await Auth.currentAuthenticatedUser();
+    let userName = user.username;
+
+    let currentTime = new Date().toLocaleString();
+    let post = {
+      text: formText,
+      time: currentTime,
+      user: userName
+    }
+    await API.graphql({ query: createNoteMutation, variables: { input: post } });
+    setPosts([ ...posts, post ]);
+    setFormText('');
+  }
+
+  function handleChange(e) {
+    setFormText(e.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Social Media Project</h1>
       </header>
-      <PostDisplay postsNow={[]} user="aelyssa" />
+      <br />
+      <PostDisplay postsNow={posts} />
+      <PostCreator formText={formText} onFormChange={handleChange} onFormSubmit={createPosts} />
       <br />
       <AmplifySignOut />
     </div>
--- src/PostCreator.jsx
+++ src/PostCreator.jsx
@@ -1,44 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 export class PostDisplay extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {posts: this.props.postsNow, formText: '', user: this.props.user};
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-  
-  handleChange(text) {
-    this.setState({formText: text})
-    
-  }
-  
-  handleSubmit() {
 
-    let currentTime = new Date().toLocaleString();
-    let post = {
-      text: this.state.formText,
-      time: currentTime,
-      user: this.props.user
-    }
-    let postCopy = this.state.posts;
-    postCopy.push(post);
-    this.setState({posts: postCopy, formText: ''});
-    
-  }
-  
   render() {
-    let display = this.state.posts.map((x, i) => {return(<div key={i}>
+    let display = this.props.postsNow.map((x, i) => {return(<div key={i}>
         <h3>{x.text}</h3>
         <div>{x.user + '    '+ x.time}</div> 
       </div>)})
     return(
-      <div>
       <div>{display}
-      </div>
-        <PostCreator formText={this.state.formText} onFormChange={this.handleChange} onFormSubmit={this.handleSubmit} />
-        </div>)
+      </div>)    
   }
 }
 
@@ -61,12 +32,9 @@ export class PostCreator extends React.Component {
   render() {
     return(
       <div>
-        <input type='text' style={{rows:'5', cols:'40'}} placeholder='Enter post here' value={this.props.formText} onChange={this.handleChange}/>
+        <input type='text' style={{rows:5, cols:40}} placeholder='Enter post here' value={this.props.formText} onChange={this.handleChange}/>
         <button onClick={this.handleSubmit}>Post</button>
         </div>
       )
   }
 }
-
-//let postsHere = [{text: "Hi this is a post", time: "12:26:47", user: "me"}, {text: "Hi this is a post", time: "12:26:47", user: "me"}];
-//ReactDOM.render(<PostDisplay postNow={postsHere} user={'me'} />, document.getElementById("root"));
