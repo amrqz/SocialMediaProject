7692c50fbb40d2dac2accaa3cb018a04efc106d7
Aelyssa Marquez <aelyssa.marquez@durham.ac.uk>
2021-04-21 11:10:57 +0100

    Begin styling page using CSS

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1>Social Media Project</h1>
       </header>
       <br />
--- src/PostCreator.jsx
+++ src/PostCreator.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './PostCreator.css';
 
 export class PostDisplay extends React.Component {
 
